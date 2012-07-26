31ea7907d9dc770a643823e2f170089e30e68e44
Antti Salonen <ajsalonen@gmail.com>
2012-07-26 22:33:40 +0200

    add some world structure

--- src/brigades/World.cpp
+++ src/brigades/World.cpp
@@ -1,10 +1,61 @@
 #include "World.h"
 
+using namespace Common;
+
 namespace Brigades {
 
-World::World()
+Tree::Tree(boost::shared_ptr<World> w)
+	: Entity<WorldPtr>(w)
 {
 }
+
+Soldier::Soldier(boost::shared_ptr<World> w)
+	: Entity<WorldPtr>(w)
+{
+}
+
+World::World()
+	: mWidth(100.0f),
+	mHeight(100.0f)
+{
+}
+
+// accessors
+std::vector<TreePtr> World::getTreesAt(const Vector3& v, float radius) const
+{
+	/* TODO */
+	return std::vector<TreePtr>();
+}
+
+std::vector<SoldierPtr> World::getSoldiersAt(const Vector3& v, float radius) const
+{
+	/* TODO */
+	return std::vector<SoldierPtr>();
+}
+
+float World::getWidth() const
+{
+	return mWidth;
+}
+
+float World::getHeight() const
+{
+	return mHeight;
+}
+
+
+// modifiers
+void World::update(float time)
+{
+	/* TODO */
+}
+
+bool World::addSoldierAction(const SoldierPtr s, const SoldierAction& a)
+{
+	/* TODO */
+	return true;
+}
+
 
 
 }
--- src/brigades/World.h
+++ src/brigades/World.h
@@ -1,15 +1,53 @@
 #ifndef BRIGADES_WORLD_H
 #define BRIGADES_WORLD_H
 
+#include <vector>
+
 #include <boost/shared_ptr.hpp>
 
+#include "common/Entity.h"
+
 namespace Brigades {
+
+class World;
+
+class Tree : public Common::Entity<boost::shared_ptr<World>> {
+	public:
+		Tree(boost::shared_ptr<World> w);
+};
+
+typedef boost::shared_ptr<Tree> TreePtr;
+
+class Soldier : public Common::Entity<boost::shared_ptr<World>> {
+	public:
+		Soldier(boost::shared_ptr<World> w);
+};
+
+typedef boost::shared_ptr<Soldier> SoldierPtr;
+
+class SoldierAction {
+};
+
+typedef boost::shared_ptr<SoldierAction> SoldierActionPtr;
 
 class World {
 
 	public:
 		World();
 
+		// accessors
+		std::vector<TreePtr> getTreesAt(const Vector3& v, float radius) const;
+		std::vector<SoldierPtr> getSoldiersAt(const Vector3& v, float radius) const;
+		float getWidth() const;
+		float getHeight() const;
+
+		// modifiers
+		void update(float time);
+		bool addSoldierAction(const SoldierPtr s, const SoldierAction& a);
+
+	protected:
+		float mWidth;
+		float mHeight;
 };
 
 typedef boost::shared_ptr<World> WorldPtr;
